ec94d90e62377e8671f9a81f00c8b257dc37449c
Gaurigithub-p <gauripawar8692@gmail.com>
2024-10-27 19:56:02 +0530

    First commit

---
+++ src/main/java/Microsoft/Msexcel/MsexcelApplication.java
@@ -0,0 +1,14 @@
+package Microsoft.Msexcel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsexcelApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		SpringApplication.run(MsexcelApplication.class, args);
+	}
+
+}
